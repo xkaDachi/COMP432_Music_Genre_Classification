16da67a8d3f0c9100ab123a7b1c7119c454d1e7a
Siu Ye <SiuTongYe@gmail.com>
2021-12-02 23:06:53 -0500

    convert keras to pytorch

--- neural_network.py
+++ neural_network.py
@@ -1,11 +1,12 @@
 import json
 import numpy as np
 from sklearn.model_selection import train_test_split
-import tensorflow.keras as keras
 import matplotlib.pyplot as plt
+import torch
 
 # path to json file that stores MFCCs and genre labels for each processed segment
 DATA_PATH = "/data.json"
+
 
 def load_data(data_path):
     """Loads training dataset from json file.
@@ -61,29 +62,32 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     # build network topology
-    model = keras.Sequential([
+    model = torch.Sequential([
 
         # input layer
-        keras.layers.Flatten(input_shape=(X.shape[1], X.shape[2])),
+        torch.flatten(input_shape=(X.shape[1], X.shape[2])),
 
         # 1st dense layer
-        keras.layers.Dense(512, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
+        torch.nn.Linear(512, activation='relu',
+                        kernel_regularizer=torch.norm(0.001, p=2)),
+        torch.nn.Dropout(0.3),
 
         # 2nd dense layer
-        keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
+        torch.nn.Linear(256, activation='relu',
+                        kernel_regularizer=torch.norm(0.001, p=2)),
+        torch.nn.Dropout(0.3),
 
         # 3rd dense layer
-        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
+        torch.nn.Linear(64, activation='relu',
+                        kernel_regularizer=torch.norm(0.001, p=2)),
+        torch.nn.Dropout(0.3),
 
         # output layer
-        keras.layers.Dense(10, activation='softmax')
+        torch.nn.Linear(10, activation='softmax')
     ])
 
     # compile model
-    optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+    optimiser = torch.optim.Adam(learning_rate=0.0001)
     model.compile(optimizer=optimiser,
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
@@ -91,7 +95,8 @@ if __name__ == "__main__":
     model.summary()
 
     # train model
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=100)
+    history = model.fit(X_train, y_train, validation_data=(
+        X_test, y_test), batch_size=32, epochs=100)
 
     # plot accuracy and error as a function of the epochs
     plot_history(history)
